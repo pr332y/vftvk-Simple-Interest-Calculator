4193949962ffc5b28e0103837bbb969db6cd2e25
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-10 11:48:41 -0600

    options
    populate options using JS, still need to remove duplicate and add default value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function yearsArray(num) {
+    let years=document.getElementById("years")
+    let year=[...Array(num+1).keys()]
+    year.shift()
+    year.forEach(element => {years.add(new Option(element))})
 }
-        
