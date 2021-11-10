046461120e38084a5d1ff4e4dd8cd7362ffcd2c0
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-10 12:09:25 -0600

    default option
    made the default option 1 year

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ function yearsArray(num) {
     let years=document.getElementById("years")
     let year=[...Array(num+1).keys()]
     year.shift()
-    year.forEach(element => {years.add(new Option(element))})
+    year.shift()
+    year.forEach(element => {
+        switch (element) {
+            case 1:
+                years.add(new Option(element,element,true))
+                break;
+            default:
+                years.add(new Option(element,element,false))
+                break;
+        }
+    })
 }
