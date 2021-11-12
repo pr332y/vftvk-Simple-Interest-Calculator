1265465c3520f2053b11377c7b12faf2157ef3f7
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-12 11:54:11 -0600

    styles and calcs
    Adding styles and initializing variables for calculations

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function yearsArray(num) {
         }
     })
 }
+let principal=document.getElementById("principal").value
+let rate=document.getElementById("rate").value
+let years=document.getElementById("years").value
+let interest = principal * years * rate /100
+let year = new Date().getFullYear()+parseInt(years)
+function updateRate() {
+    let rate=document.getElementById("rate").value
+    document.getElementById("rate_val").innerText=rate
+}
