d8badbb4627fe4141f77ef0f75110eb5aac5ccde
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-13 11:12:30 -0600

    rate output and result calcs
    rate output from range slider and result calculations

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ function yearsArray(num) {
         }
     })
 }
-let principal=document.getElementById("principal").value
-let rate=document.getElementById("rate").value
-let years=document.getElementById("years").value
-let interest = principal * years * rate /100
-let year = new Date().getFullYear()+parseInt(years)
-function updateRate() {
-    let rate=document.getElementById("rate").value
-    document.getElementById("rate_val").innerText=rate
+function compute() {
+    let principal=document.getElementById("principal")
+    let rate=document.getElementsByTagName("output")[0]
+    let yearNum=document.getElementById("years")
+    let amount = principal * years * rate /100
+    let futureYear = new Date().getFullYear()+parseInt(years)
+    let result=document.getElementById("result")
+    result.innerHTML=`If you deposit ${principal} <br>
+                    at a rate of ${rate} <br> 
+                    You will receive an amount of ${amount} <br> 
+                    in the year ${futureYear}`
 }
