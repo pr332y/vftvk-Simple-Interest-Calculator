c7e097e09f6b00f56c99787820e07f46d46dd2e0
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-14 12:06:58 -0600

    calculations
    connect dropdown to js function and get calculations working

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+document.getElementsByTagName("select")[0].addEventListener("change",yearsArray(10))
+document.getElementById("calculate").addEventListener("click",()=>{
+    let principal=parseInt(document.getElementById("principal").value)
+    let rate=parseInt(document.getElementsByTagName("output")[0].value)
+    let years=parseInt(document.getElementById("years").value)
+    let interest = principal * years * rate /100
+    let total=interest+principal
+    let futureYear = new Date().getFullYear()+parseInt(years)
+    let result=document.getElementById("result")
+    result.innerHTML=`If you deposit ${principal},\<br\>
+    at a rate of ${rate}%\<br\> 
+    You will receive an amount of ${total} ,\<br\> 
+    in the year ${futureYear}`
+    result.style.display="block"
+})
 function yearsArray(num) {
     let years=document.getElementById("years")
     let year=[...Array(num+1).keys()]
-    year.shift()
     year.shift()
     year.forEach(element => {
         switch (element) {
@@ -13,16 +27,4 @@ function yearsArray(num) {
                 break;
         }
     })
-}
-function compute() {
-    let principal=document.getElementById("principal")
-    let rate=document.getElementsByTagName("output")[0]
-    let yearNum=document.getElementById("years")
-    let amount = principal * years * rate /100
-    let futureYear = new Date().getFullYear()+parseInt(years)
-    let result=document.getElementById("result")
-    result.innerHTML=`If you deposit ${principal} <br>
-                    at a rate of ${rate} <br> 
-                    You will receive an amount of ${amount} <br> 
-                    in the year ${futureYear}`
 }
