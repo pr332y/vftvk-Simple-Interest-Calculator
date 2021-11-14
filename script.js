26cdc2061e4b93c07aff22383e4a225752a43662
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-14 15:21:09 -0600

    corrected calculations
    correct calculations of principal, rate, interest

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 document.getElementsByTagName("select")[0].addEventListener("change",yearsArray(10))
 document.getElementById("calculate").addEventListener("click",()=>{
-    let principal=parseInt(document.getElementById("principal").value)
-    let rate=parseInt(document.getElementsByTagName("output")[0].value)
+    let principal=Number(document.getElementById("principal").value)
+    let rate=Number(document.getElementsByTagName("output")[0].value)
     let years=parseInt(document.getElementById("years").value)
     let interest = principal * years * rate /100
-    let total=interest+principal
     let futureYear = new Date().getFullYear()+parseInt(years)
     let result=document.getElementById("result")
     result.innerHTML=`If you deposit ${principal},\<br\>
     at a rate of ${rate}%\<br\> 
-    You will receive an amount of ${total} ,\<br\> 
+    You will receive an amount of ${interest} ,\<br\> 
     in the year ${futureYear}`
     result.style.display="block"
 })
