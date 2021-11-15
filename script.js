dc13e1ef263cfa60101b123c49b7eff9d89d8390
pr332y <35409036+pr332y@users.noreply.github.com>
2021-11-14 23:07:52 -0600

    comments and form validation
    add comments and validation for principal field

--- script.js
+++ script.js
@@ -4,20 +4,29 @@ document.getElementById("calculate").addEventListener("click",()=>{
     let rate=Number(document.getElementsByTagName("output")[0].value)
     let years=parseInt(document.getElementById("years").value)
     let interest = principal * years * rate /100
-    let futureYear = new Date().getFullYear()+parseInt(years)
+    let futureYear = new Date().getFullYear()+years
     let result=document.getElementById("result")
-    result.innerHTML='If you deposit <span class="highlighted">'+principal+'</span><br/>'+
-                     'at a rate of <span class="highlighted">'+rate+'%</span><br/>'+
-                     'You will receive an amount of <span class="highlighted">'+interest+'</span><br/>'+
-                     'in the year <span class="highlighted">'+futureYear+'</span><br/>'
-    result.style.display="block"
+    if (principal<=0) {
+        alert('Enter a positive number')
+        document.getElementById('principal').focus()
+    } else {
+        //display result and add html elements with class called highlighted to target for css formatting
+        result.innerHTML='If you deposit <span class="highlighted">'+principal+'</span><br/>'+
+                         'at a rate of <span class="highlighted">'+rate+'%</span><br/>'+
+                         'You will receive an amount of <span class="highlighted">'+interest+'</span><br/>'+
+                         'in the year <span class="highlighted">'+futureYear+'</span><br/>'
+        result.style.display="block"        
+    }
+    
 })
+//Dynamically create new option items for years input
 function yearsArray(num) {
     let years=document.getElementById("years")
     let year=[...Array(num+1).keys()]
     year.shift()
     year.forEach(element => {
         switch (element) {
+            //use 1 as the default selection
             case 1:
                 years.add(new Option(element,element,true))
                 break;
